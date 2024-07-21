c0a750189485e7bdc13dea085967a2943e3c53b1
viniciius14 <rodri.f.ribeiro@gmail.com>
2024-07-21 14:06:57 +0100

    New OS structure and stage2 bootloader is loaded into RAM.

--- docs/references.md
+++ docs/references.md
@@ -3,3 +3,9 @@ https://littleosbook.github.io/
 https://wiki.osdev.org/
 https://www.stanislavs.org/helppc/int_13.html
 https://c9x.me/x86/
+https://stackoverflow.com/questions/67613202/bios-interrupt-13-read-sector-not-working
+https://stackoverflow.com/questions/4590078/how-can-i-write-directly-to-the-screen/4591232#4591232
+http://www.cs.cmu.edu/~410-s07/p4/p4-boot.pdf
+https://fileadmin.cs.lth.se/cs/Education/EDA385/HT09/student_doc/FinalReports/FAT12_overview.pdf
+https://academy.cba.mit.edu/classes/networking_communications/SD/FAT.pdf
+http://www.brokenthorn.com/Resources/OSDev6.html
--- make.sh
+++ make.sh
@@ -1,23 +1,64 @@
 # Make sure bin directory exists
 mkdir -p bin/
 
-# Stage 1 Bootloader
-nasm -f bin src/bootloader/stage1.S -o bin/stage1.bin -i src/filesystems/FAT/FAT_Boot -i src/bootloader
 # Stage 2 Bootloader
+nasm src/bootloader/stage2.S        \
+    -i src/bootloader/              \
+    -f bin                          \
+    -o bin/stage2.bin
 
 # Kernel
-nasm -f bin src/kernel/kernel.S -o bin/kernel.bin
+# nasm src/kernel/kernel.S            \
+#     -i src/bootloader/              \
+#     -f bin                          \
+#     -o bin/kernel.bin
+
+# Get size of Stage 2 Bootloader + kernel to know how many sectors to reserve
+STAGE2_SIZE=$(stat -c %s bin/stage2.bin)
+KERNEL_SIZE=$(stat -c %s bin/kernel.bin)
+
+echo "Stage 2 size = $STAGE2_SIZE"
+echo "Kernel size  = $KERNEL_SIZE"
+
+RSV_SCTRS=$((STAGE2_SIZE + KERNEL_SIZE))
+
+# Check if we ocuppy an uneven number of sectors and add the default reserved sector
+if [ $((RSV_SCTRS % 512)) -ne 0 ]; then
+    RSV_SCTRS=$(((RSV_SCTRS / 512) + 2))
+else
+    RSV_SCTRS=$(((RSV_SCTRS / 512) + 1))
+fi
+
+echo "Reserved sectors = $RSV_SCTRS"
+
+# Stage 1 Bootloader
+nasm src/bootloader/stage1.S        \
+    -DRESERVED_SECTORS=$RSV_SCTRS   \
+    -i src/filesystems/FAT/FAT_Boot \
+    -i src/bootloader               \
+    -f bin                          \
+    -o bin/stage1.bin
+
+cat bin/stage1.bin bin/stage2.bin > bin/bootloader.bin
+# cat bin/bootloader.bin bin/kernel.bin > GeckOS.bin
 
 # Create virtual floppy disk
 dd if=/dev/zero of=bin/GeckOS.img bs=512 count=2880
-mkfs.fat -F 12 -n "GECKOS" bin/GeckOS.img
-dd if=bin/stage1.bin of=bin/GeckOS.img conv=notrunc
 
-# Add kernel to floppy
-mcopy -i bin/GeckOS.img bin/kernel.bin "::kernel.bin"
+mkfs.fat bin/GeckOS.img             \
+    -F 12                           \
+    -n "GECKOS"                     \
+    -R $RSV_SCTRS
+
+dd if=bin/bootloader.bin of=bin/GeckOS.img bs=512 seek=0 conv=notrunc
+# dd if=bin/GeckOS.bin of=bin/GeckOS.img bs=512 seek=0 conv=notrunc
+
+# Add files
+mcopy -i bin/GeckOS.img test.txt "::test.txt"
 
 # Emulate
 qemu-system-x86_64 -drive format=raw,file=bin/GeckOS.img,index=0,if=floppy
 
 # Cleanup
-rm bin/*
+rm -f bin/*.bin
+rm -f bin/*.tags
