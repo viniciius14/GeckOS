abb9d57378843f24421ca40d31320ade2c34ffc9
viniciius14 <rodri.f.ribeiro@gmail.com>
2024-08-13 20:43:53 +0100

    Preparation for floppy driver work

--- make.sh
+++ make.sh
@@ -1,28 +1,17 @@
-# Stage 2 Bootloader
-nasm src/bootloader/stage2.S        \
-    -i src/bootloader/              \
-    -f elf32                        \
-    -o bin/stage2.elf
+# Variables
+KERNEL_SECTORS=-1
+BOOT_2_SECTORS=-1
 
-nasm src/bootloader/stage2_32.S     \
-    -i src/bootloader/              \
-    -f elf32                        \
-    -o bin/stage2_32.elf
+# Ensure that bin/ exists
+mkdir -p bin/
 
-ld bin/stage2.elf bin/stage2_32.elf \
-    -m elf_i386                     \
-    -Ttext 0x7E00                   \
-    -e _stage2_start                \
-    -o bin/stage2.o
 
-objcopy bin/stage2.o                \
-    -O binary                       \
-    bin/stage2.bin
+# We're gonna compile eveything the opposite way around
 
 # Kernel
 i386-elf-gcc                        \
     -c src/kernel/kernel_main.c     \
-    -o bin/kernel.elf               \
+    -o bin/_kernel.o                \
     -Wall                           \
     -Wextra                         \
     -m32                            \
@@ -30,51 +19,74 @@ i386-elf-gcc                        \
     -fno-builtin                    \
     -mgeneral-regs-only
 
-ld bin/kernel.elf                   \
+i386-elf-ld                         \
+    bin/_kernel.o                   \
     -m elf_i386                     \
-    -Ttext 0x1000000                \
-    -e _kernel_main                 \
+    -T misc/kernel.ld               \
     -o bin/kernel.o
 
-
-objcopy bin/kernel.o                \
+i386-elf-objcopy                    \
+    bin/kernel.o                    \
     -O binary                       \
     bin/kernel.bin
 
+get_sectors KERNEL_SECTORS bin/kernel.bin
 
 
-# # Kernel
-# # nasm src/kernel/kernel.S            \
-# #     -i src/bootloader/              \
-# #     -f bin                          \
-# #     -o bin/kernel.bin
+# Stage 2 Bootloader
+nasm                                \
+    src/bootloader/stage2.S         \
+    -i src/bootloader/              \
+    -f elf32                        \
+    -o bin/_stage2.o
 
-# Get size of Stage 2 Bootloader + kernel to know how many sectors to reserve
-STAGE2_SIZE=$(stat -c %s bin/stage2.bin)
-KERNEL_SIZE=$(stat -c %s bin/kernel.bin)
+nasm                                \
+    src/bootloader/stage2_32.S      \
+    -i src/bootloader/              \
+    -f elf32                        \
+    -o bin/_stage2_32.o
 
-echo "Stage 2 size = $STAGE2_SIZE"
-echo "Kernel size  = $KERNEL_SIZE"
+# i386-elf-gcc                        \
+#     -c src/bootloader/stage2.S      \
+#     -I src/bootloader/              \
+#     -o bin/_stage2.o                \
+#     -Wall                           \
+#     -Wextra                         \
+#     -nostdlib                       \
+#     -fno-builtin                    \
+#     -mgeneral-regs-only
 
-RSV_SCTRS=$((STAGE2_SIZE + KERNEL_SIZE))
+i386-elf-ld                         \
+    bin/_stage2.o                   \
+    bin/_stage2_32.o                \
+    -m elf_i386                     \
+    -T misc/bootloader.ld           \
+    --no-warn-rwx-segments          \
+    -o bin/stage2.o
 
-# Check if we ocuppy an uneven number of sectors and add the default reserved sector
-if [ $((RSV_SCTRS % 512)) -ne 0 ]; then
-    RSV_SCTRS=$(((RSV_SCTRS / 512) + 2))
-else
-    RSV_SCTRS=$(((RSV_SCTRS / 512) + 1))
-fi
+i386-elf-objcopy                    \
+    bin/stage2.o                    \
+    -O binary                       \
+    bin/stage2.bin
 
-echo "Reserved sectors = $RSV_SCTRS"
+get_sectors BOOT_2_SECTORS bin/stage2.bin
+
 
 # Stage 1 Bootloader
-nasm src/bootloader/stage1.S        \
-    -DRESERVED_SECTORS=$RSV_SCTRS   \
-    -i src/filesystems/FAT/FAT_Boot \
-    -i src/bootloader               \
-    -f bin                          \
+nasm                                    \
+    src/bootloader/stage1.S             \
+    -DBOOT_2_SECTORS=$KERNEL_SECTORS    \
+    -DKERNEL_SECTORS=$BOOT_2_SECTORS    \
+    -i src/filesystems/FAT/FAT_Boot     \
+    -i src/bootloader                   \
+    -f bin                              \
     -o bin/stage1.bin
 
+
+RESERVED_SECTORS=$((KERNEL_SECTORS + BOOT_2_SECTORS + 1))
+echo "RESERVED SECTORS = $RESERVED_SECTORS"
+
+# "Join" together all the parts
 cat bin/stage1.bin bin/stage2.bin > bin/bootloader.bin
 cat bin/bootloader.bin bin/kernel.bin > bin/GeckOS.bin
 
@@ -84,9 +96,8 @@ dd if=/dev/zero of=bin/GeckOS.img bs=512 count=2880
 mkfs.fat bin/GeckOS.img             \
     -F 12                           \
     -n "GECKOS"                     \
-    -R $RSV_SCTRS
+    -R $RESERVED_SECTORS
 
-# dd if=bin/bootloader.bin of=bin/GeckOS.img bs=512 seek=0 conv=notrunc
 dd if=bin/GeckOS.bin of=bin/GeckOS.img bs=512 seek=0 conv=notrunc
 
 # Add files
@@ -96,7 +107,33 @@ mcopy -i bin/GeckOS.img test.txt "::test.txt"
 qemu-system-x86_64 -drive format=raw,file=bin/GeckOS.img,index=0,if=floppy
 
 # Cleanup
-rm -f bin/*.bin
-rm -f bin/*.tags
-rm -f bin/*.elf
-rm -f bin/*.o
+# rm -f bin/*.bin
+# rm -f bin/*.tags
+# rm -f bin/*.elf
+# rm -f bin/*.o
+
+
+
+# Functions
+
+get_sectors() {
+    local var_name=$1
+    local file_path=$2
+
+    # Get the size of the file
+    local file_size=$(stat -c %s "$file_path")
+
+    echo "$file_path is $file_size bytes long."
+
+    # Check if we ocuppy an uneven number of sectors
+    if [ $((file_size % 512)) -ne 0 ]; then
+        file_size=$(((file_size / 512) + 1))
+    else
+        file_size=$(((file_size / 512)))
+    fi
+
+    echo "$file_path is $file_size sectors long."
+
+    # Assign the value to the variable passed as argument
+    eval "$var_name=$file_size"
+}
--- src/kernel/kernel_main.c
+++ src/kernel/kernel_main.c
@@ -1,7 +1,9 @@
 
 void _kernel_main(void) {
-    unsigned char * mem = (unsigned char *)0xB8000;
+    volatile unsigned char * mem = (unsigned char *)0xB8000;
     mem[0] = 'B';
+    volatile char* a = "This is my string";
+    mem[4] = a[0];
 
     for (;;){}
 }
